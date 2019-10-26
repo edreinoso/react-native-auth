45e9e07972eb0a2253f691bef6d31ab2686997a7
Ed <edgardojesus16@gmail.com>
2019-10-26 12:27:40 -0400

    finally got done with input component, now need to understand it a bit further. Maybe place reducers in different classes

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,54 +1,136 @@
-import React, { Component } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { colors } from '../styles/index';
 
-class TextField extends Component {
-  render() {
-    const {
-      title,
-      placeholder,
-      placeholderTextColor,
-      borderColor,
-      borderWidth,
-      borderRadius,
-      value,
-      onChangeText,
-      autoCorrect,
-      autoCapitalize,
-      height,
-      width,
-      color
-    } = this.props;
+const INPUT_CHANGE = 'INPUT_CHANGE';
+const INPUT_BLUR = 'INPUT_BLUR';
 
-    return (
-      <View>
-        <Text>{title}</Text>
-        <TextInput
-          placeholder={placeholder ? placeholder : "Text Input"}
-          value={value}
-          onChangeText={onChangeText}
-          autoCapitalize={autoCapitalize}
-          autoCorrect={autoCorrect}
-          style={[styles.paddingHorizontal,{
-            height: height,
-            width: width,
-            color: color,
-            borderColor: borderColor,
-            borderWidth: borderWidth,
-            borderRadius: borderRadius,
-            padding: 5,
-          }]}
-        />
-      </View>
-    );
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case INPUT_CHANGE:
+      return {
+        ...state,
+        value: action.value,
+        isValid: action.isValid
+      };
+    case INPUT_BLUR:
+      return {
+        ...state,
+        touched: true
+      }
+    default:
+      return state;
   }
 }
 
+const TextField = props => { // hook problems when having classes
+  const {
+    title,
+    placeholder,
+    borderColor,
+    borderWidth,
+    initialValue,
+    initiallyValid,
+    email,
+    errorText,
+    onChangeInput, // this is onInputChange
+    id,
+    autoCorrect,
+    autoCapitalize,
+    color,
+    minLength,
+    secureTextEntry,
+    keyboardType,
+    required, //this value represent required fields
+    placeholderTextColor,
+    borderRadius,
+  } = props;
+
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: initialValue ? initialValue : '',
+    isValid: initiallyValid, // where is this coming from
+    touched: false
+  });
+
+  useEffect(() => {
+    if (inputState.touched) {
+      onChangeInput(id, inputState.value, inputState.isValid)
+    }
+  }, [inputState, onChangeInput, id])
+
+  // variable text would be obteined from getting the onChangeText down below
+  const textChangeHandler = text => {
+    const emailCheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let isValid = true;
+
+    // manual validation tests
+    if (required && text.trim().length === 0) {
+      isValid = false;
+    } if (email && !emailCheck.test(text.toLowerCase())) {
+      isValid = false;
+    } if (minLength != null && text.length < minLength) {
+      isValid = false;
+    } if (minLength != null && text.length < minLength) {
+      isValid = false;
+    }
+    dispatch({
+      type: INPUT_CHANGE,
+      value: text,
+      isValid: isValid
+    });
+  }
+
+  const lostFocusHandler = () => {
+    dispatch({ type: INPUT_BLUR })
+  }
+
+  return (
+    <View style={styles.formControler}>
+      <Text style={styles.label}>{title}</Text>
+      <TextInput
+        placeholder={placeholder ? placeholder : ""}
+        value={inputState.value}
+        onChangeText={textChangeHandler}
+        autoCapitalize={autoCapitalize}
+        keyboardType={keyboardType}
+        autoCorrect={autoCorrect}
+        secureTextEntry={secureTextEntry}
+        onBlur={lostFocusHandler}
+        style={[styles.inputPadding, {
+          color: color,
+          borderBottomColor: borderColor,
+          borderBottomWidth: borderWidth,
+        }]}
+      />
+      {!inputState.isValid && inputState.touched && (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{errorText}</Text>
+        </View> //it is not <View/>
+      )}
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
-  paddingHorizontal: {
+  formControl: {
+    width: '100%'
+  },
+  label: {
+    marginHorizontal: 5
+  },
+  inputPadding: {
+    paddingVertical: 5,
     paddingHorizontal: 10,
   },
   bottomPadding: {
     paddingBottom: 2,
+  },
+  errorContainer: {
+    marginVertical: 5
+  },
+  errorText: {
+    color: colors.red,
+    fontSize: 13
   }
 });
 
--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,25 +1,123 @@
-import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { container, text, colors, dimensions } from '../styles/index';
+import React, { useReducer, useCallback } from 'react';
+import { View, ScrollView } from 'react-native';
+import { container, colors } from '../styles/index';
 import { Cards, Button, TextField } from '../components/index';
 import { LinearGradient } from 'expo-linear-gradient';
+// import { useDispatch } from 'react-redux';
 
+const AuthScreen = props => {
+  // this can be put into a different file
 
-class AuthScreen extends Component {
-  render() {
-    return (
-      // this flex is necessary for persistency
-      <View style={container.screen}>
-        <LinearGradient colors={['#ffedff', '#ffe3ff']} style={container.gradientContainer}>
-          <Cards style={container.authContainer}>
-            <ScrollView>
-              <Text>Hello World</Text>
-            </ScrollView>
-          </Cards>
-        </LinearGradient>
-      </View>
-    );
+  const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
+
+  const formReducer = (state, action) => {
+    if (action.type === FORM_INPUT_UPDATE) {
+      const updatedValues = {
+        ...state.inputValues,
+        [action.input]: action.value
+      };
+      const updatedValidities = {
+        ...state.inputValidities,
+        [action.input]: action.isValid
+      };
+      let updatedFormIsValid = true;
+      // what is this expression?
+      for (const key in updatedValidities) {
+        updatedFormIsValid = updatedFormIsValid && updatedValidities[key]
+      }
+      return {
+        formIsValid: updatedFormIsValid,
+        inputValidities: updatedValidities,
+        inputValues: updatedValues
+      };
+    }
+    return state;
   }
+
+  // const dispatch = useDispatch();
+
+  // why joining two values in an object
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      email: '',
+      password: ''
+    },
+    inputValidities: {
+      email: false,
+      password: false,
+    },
+    formIsValid: false
+  })
+
+  // const signUpHandler = () => {
+  //   dispatch(
+  //     authActions.signup(
+  //       formState.inputValues.email,
+  //       formState.inputValues.password
+  //     )
+  //   )
+  // }
+
+  // why is usecallback here?
+  const onChangeTextHandler = useCallback((inputIdentifier, inputValue, inputValidity) => {
+    dispatchFormState({
+      type: FORM_INPUT_UPDATE,
+      value: inputValue,
+      isValid: inputValidity,
+      input: inputIdentifier
+    });
+  }, [dispatchFormState]);
+
+  return (
+    // this flex is necessary for persistency
+    <View style={container.screen}>
+      <LinearGradient colors={['#ffedff', '#ffe3ff']} style={container.gradientContainer}>
+        <Cards style={container.authContainer}>
+          <ScrollView>
+            <TextField
+              title={'Email'}
+              color={colors.black}
+              borderColor={colors.black}
+              borderWidth={1}
+              onChangeInput={onChangeTextHandler}
+              initialValue=''
+              errorText='Please enter valid email'
+              required
+              keyboardType='email-address'
+              autoCapitalize='none'
+            />
+            <View style={{padding: 10}}/> {/* Is this really necessary? */}
+            <TextField
+              title={'Password'}
+              color={colors.black}
+              borderColor={colors.black}
+              borderWidth={1}
+              minLength={5}
+              onChangeInput={onChangeTextHandler}
+              initialValue=''
+              errorText='Please enter valid password'
+              keyboardType='default'
+              required
+              secureTextEntry
+            />
+            {/* <Button
+                fontSize={text.buttonText}
+                width={dimensions.width / 3}
+                // height={20}
+                borderWidth={1}
+                padding={10}
+                color={colors.white}
+                textColor={colors.blue}
+                borderColor={colors.white}
+                borderRadius={5}
+                text='button 1'
+                onButtonPress={() => this.props.navigation.navigate('Fourth')}
+              /> */}
+          </ScrollView>
+        </Cards>
+      </LinearGradient>
+    </View>
+  );
 }
 
 export default AuthScreen;
