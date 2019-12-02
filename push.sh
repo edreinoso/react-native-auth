2e87d7475df651dc5feea9134473197f2c0fc097
Ed <edgardojesus16@gmail.com>
2019-12-02 16:44:11 -0500

    testing the push file 2

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git add .
 read -p "Enter commit message: " message
 git commit -m "$message"
 git branch -a
-echo -p "Which branch you want to push? " branch
+read -p "Which branch you want to push? " branch
 git push origin $branch
