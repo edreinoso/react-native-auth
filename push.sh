0faae318ebb50503e62942f6dba6c7b2e09e593a
Ed <edgardojesus16@gmail.com>
2019-12-04 11:59:20 -0500

    installed amplify in the project

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git add .
 read -p "Enter commit message: " message
 git commit -m "$message"
 git branch -a
-read "Do you want to push? " decision
+read -p "Do you want to push? " decision
 if [ $decision == "y" ]; then
   read -p "Which branch you want to push? " branch
   git push origin $branch
