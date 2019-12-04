b1c9a2deb65e5b852b957fd7089b9cafa540df1f
Ed <edgardojesus16@gmail.com>
2019-12-04 13:36:07 -0500

    aws-amplify-react-native installation

--- App.js
+++ App.js
@@ -1,29 +1,10 @@
 import React, { Component } from 'react';
-import { useScreens } from 'react-native-screens';
-// import Root from './src/redux';
-
-// import { createStore, combineReducers } from 'redux';
-// import { Provider } from 'react-redux';
-
 import ScreenNavigators from './src/navigation/ScreenNavigators';
-
-useScreens();
-
-// Reducers should goes inside of ./src/store/index.js file
-// const rootReducer = combineReducers({})
-
-// Store should initialization goes inside of ./src/redux.js
-// const store = createStore();
 
 export default class App extends Component {
   render() {
     return (
-      // <Provider store={store}>
       <ScreenNavigators />
-      // </Provider>
-      // <Root>
-      // <ScreenNavigators />
-      // </Root>
     )
   }
 }
